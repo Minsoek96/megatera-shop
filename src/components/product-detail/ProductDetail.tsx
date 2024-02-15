a93ed7f2d2092ecc530cb2ce2bb676954e03eb5a
Minsoek96 <fe.minseok96@gmail.com>
2024-02-16 01:40:24 +0900

    feat : 상품 상세 추가

---
+++ src/components/product-detail/ProductDetail.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+import useProductDetailStore from '../../hooks/useProductDetailStore';
+import Images from './Image';
+import Description from './Description';
+
+function AddToCartForm() {
+  return null;
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  aside {
+    width: 38%;
+  }
+
+  article {
+    width: 60%;
+  }
+`;
+
+export default function ProductDetailView() {
+  const [{ product }] = useProductDetailStore();
+
+  return (
+    <Container>
+      <aside>
+        <Images images={product.images} />
+      </aside>
+      <article>
+        <h2>{product.name}</h2>
+        <AddToCartForm />
+        <Description value={product.description} />
+      </article>
+    </Container>
+  );
+}
